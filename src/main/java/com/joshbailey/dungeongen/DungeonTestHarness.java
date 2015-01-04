6d0bb39636742566cbb61bbfaced277786aa7fbf
Paul Kimbrel <paul@paulkimbrel.com>
2015-01-04 13:53:03 -0600

    Playing around with huge dungeons

--- src/main/java/com/joshbailey/dungeongen/DungeonTestHarness.java
+++ src/main/java/com/joshbailey/dungeongen/DungeonTestHarness.java
@@ -19,18 +19,20 @@ public class DungeonTestHarness {
 	 * <><><><><><><><><><><><> Generated dungeon<><><><><><><><><><><><>
 	 */
 
-	final int DUNGEON_SIZE_X = 50;
-	final int DUNGEON_SIZE_Y = 50;
+	final int DUNGEON_SIZE_X = 64;
+	final int DUNGEON_SIZE_Y = 64;
 
-	final int MAX_ROOM_SIZE_X = 5;
-	final int MAX_ROOM_SIZE_Y = 5;
+	final int MIN_ROOM_SIZE_X = 5;
+	final int MIN_ROOM_SIZE_Y = 5;
+	final int MAX_ROOM_SIZE_X = 13;
+	final int MAX_ROOM_SIZE_Y = 13;
 
 	Dungeon dungeon = new Dungeon(new TwoDimensionalCoordinate(DUNGEON_SIZE_X, DUNGEON_SIZE_Y));
 	int consecutiveFailedPlacements = 0;
 	do {
 	    Room newRoom = new Room(new TwoDimensionalCoordinate(randInt(0, DUNGEON_SIZE_X), randInt(0, DUNGEON_SIZE_Y)),
-		    randInt(1, MAX_ROOM_SIZE_X),
-		    randInt(1, MAX_ROOM_SIZE_Y));
+		    randInt(MIN_ROOM_SIZE_X, MAX_ROOM_SIZE_X),
+		    randInt(MIN_ROOM_SIZE_Y, MAX_ROOM_SIZE_Y));
 	    if (dungeon.ableToAccomodateNewRoom(newRoom)) {
 		dungeon.getRooms().add(newRoom);
 		consecutiveFailedPlacements = 0;
